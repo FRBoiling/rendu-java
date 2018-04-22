dc9706f9bd7921c5fa7d7d52963436f83aee37f0
fanrong <FRee22858@live.com>
2018-04-23 01:32:25 +0800

    update

---
+++ .idea/fileTemplates/includes/File Header.java
@@ -0,0 +1,10 @@
+/**
+ * Copyright © ${YEAR} ${MONTH_NAME_SHORT}
+ * Boil blood. All rights reserved.
+ * @Prject: ${PROJECT_NAME}
+ * @Package: ${PACKAGE_NAME}
+ * @Description: ${todo}
+ * @author: ${USER}  
+ * @date: ${DATE} ${TIME}
+ * @version: V1.0  
+ */
---
+++ src/main/java/core/network/codec/PacketDecoder.java
@@ -0,0 +1,30 @@
+package core.network.codec;
+
+import io.netty.buffer.ByteBuf;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.handler.codec.ByteToMessageDecoder;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.List;
+
+/**
+ * 数据包解码器
+ * @author boiling
+ */
+public class PacketDecoder extends ByteToMessageDecoder {
+    private static final Logger LOGGER = LoggerFactory.getLogger(PacketDecoder.class);
+    private static int count = 0;
+    protected void decode(ChannelHandlerContext channelHandlerContext, ByteBuf in, List<Object> out) throws Exception {
+        int length = in.readableBytes();
+        int msg_id = in.readIntLE();
+
+        ByteBuf msg = in.readBytes(length - in.readerIndex());
+
+        Packet packet = new Packet();
+        packet.setMsgId(msg_id);
+        packet.setMsg(msg);
+        out.add(packet);
+        LOGGER.debug("received message total count {0}", ++count);
+    }
+}
