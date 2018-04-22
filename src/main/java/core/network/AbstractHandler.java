64411d0c039427910e642b3ad1a90e48ecefa8cd
fanrong <FRee22858@live.com>
2018-04-23 00:45:10 +0800

    update

---
+++ src/main/java/core/network/AbstractHandler.java
@@ -0,0 +1,15 @@
+package core.network;
+
+/**
+ * Copyright © 2018 四月
+ * Boil blood. All rights reserved.
+ *
+ * @Prject: ServerCluster-Java
+ * @Package: core.network
+ * @Description: ${todo}
+ * @author: Boiling
+ * @date: 2018/4/23 0023 0:44
+ * @version: V1.0
+ */
+public class AbstractHandler {
+}
