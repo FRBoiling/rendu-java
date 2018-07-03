6001b7edc84d892267fa0ab1b402d4dd2568fb78
Boiling <FRee22858@live.com>
2018-07-03 11:37:55 +0800

    xml 解析

---
+++ common/configuration/src/test/java/configuration/datamanager/DataListTest.java
@@ -0,0 +1,195 @@
+package configuration.datamanager; 
+
+import org.junit.Test; 
+import org.junit.Before; 
+import org.junit.After; 
+
+/** 
+* DataList Tester. 
+* 
+* @author <Authors name> 
+* @since <pre>07/03/2018</pre> 
+* @version 1.0 
+*/ 
+public class DataListTest { 
+
+@Before
+public void before() throws Exception { 
+} 
+
+@After
+public void after() throws Exception { 
+} 
+
+/** 
+* 
+* Method: init(String id) 
+* 
+*/ 
+@Test
+public void testInit() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+/** 
+* 
+* Method: entrySet() 
+* 
+*/ 
+@Test
+public void testEntrySet() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+/** 
+* 
+* Method: addData(Data data) 
+* 
+*/ 
+@Test
+public void testAddData() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+/** 
+* 
+* Method: getData(String name) 
+* 
+*/ 
+@Test
+public void testGetDataName() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+/** 
+* 
+* Method: getData(int id) 
+* 
+*/ 
+@Test
+public void testGetDataId() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+/** 
+* 
+* Method: getDataListById() 
+* 
+*/ 
+@Test
+public void testGetDataListById() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+/** 
+* 
+* Method: getDataListByName() 
+* 
+*/ 
+@Test
+public void testGetDataListByName() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+/** 
+* 
+* Method: getKey() 
+* 
+*/ 
+@Test
+public void testGetKey() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+/** 
+* 
+* Method: getValue() 
+* 
+*/ 
+@Test
+public void testGetValue() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+/** 
+* 
+* Method: setValue(V var1) 
+* 
+*/ 
+@Test
+public void testSetValueValue() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+/** 
+* 
+* Method: equals(Object var1) 
+* 
+*/ 
+@Test
+public void testEqualsO() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+/** 
+* 
+* Method: hashCode() 
+* 
+*/ 
+@Test
+public void testHashCode() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+/** 
+* 
+* Method: toString() 
+* 
+*/ 
+@Test
+public void testToString() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+/** 
+* 
+* Method: comparingByKey() 
+* 
+*/ 
+@Test
+public void testComparingByKey() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+/** 
+* 
+* Method: comparingByValue() 
+* 
+*/ 
+@Test
+public void testComparingByValue() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+/** 
+* 
+* Method: comparingByKey(Comparator<? super K> var0) 
+* 
+*/ 
+@Test
+public void testComparingByKeyCmp() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+/** 
+* 
+* Method: comparingByValue(Comparator<? super V> var0) 
+* 
+*/ 
+@Test
+public void testComparingByValueCmp() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+
+} 
